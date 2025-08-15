3178931bc4659dfa207c05f58359d82c940e39e7
Tyler Forgione <tyler.forgione26@gmail.com>
2025-08-15 17:20:49 -0400

    Adds alpha beta pruning to the AI

--- connect4.py
+++ connect4.py
@@ -13,6 +13,9 @@ def current_player(board):
     return RED if count % 2 == 0 else YELLOW
 
 def actions(board):
+    """
+    Returns the set of all possible actions (i, j) available on the board.
+    """
     actions = set()
     for col in range(7):
         if board[0][col] == EMPTY:
@@ -49,12 +52,12 @@ def winner(board):
     # diagonals
     for row in range(3):
         for col in range(4):
-            # Down-right diagonal (\)
+            # down-right diagonal (\)
             if board[row][col] != EMPTY:
                 if all(board[row + i][col + i] == board[row][col] for i in range(4)):
                     return board[row][col]
 
-            # Down-left diagonal (/)
+            # down-left diagonal (/)
             if board[row][col + 3] != EMPTY:
                 if all(board[row + i][col + 3 - i] == board[row][col + 3] for i in range(4)):
                     return board[row][col + 3]
@@ -80,29 +83,34 @@ def evaluate(board):
         return utility(board)
 
     score = 0
-
     score += evaluate_windows(board, RED) - evaluate_windows(board, YELLOW)
 
     return score
 
 def evaluate_windows(board, player):
+    """
+    Determines all possible scoring windows based on the current state of the board.
+    """
     score = 0
-
+    # horizontal
     for row in range(6):
         for col in range(4):
             window = [board[row][col + i] for i in range(4)]
             score += score_window(window, player)
 
+    # vertical
     for col in range(7):
         for row in range(3):
             window = [board[row+i][col] for i in range(4)]
             score += score_window(window, player)
 
+    # down-right diag
     for row in range(3):
         for col in range(4):
             window = [board[row+i][col+i] for i in range(4)]
             score += score_window(window, player)
 
+    # down-left diag
     for row in range(3):
         for col in range(3, 7):
             window = [board[row+i][col-i] for i in range(4)]
@@ -111,6 +119,9 @@ def evaluate_windows(board, player):
     return score
 
 def score_window(window, player):
+    """
+    Returns the score of the given 4 tile window for the given player.
+    """
     score = 0
     opp = YELLOW if player == RED else RED
 
@@ -118,9 +129,11 @@ def score_window(window, player):
     empty_count = window.count(EMPTY)
     opp_count = window.count(opp)
 
+    # if opponent has a piece within scoring window, you can't score (0 points)
     if opp_count > 0:
         return 0
 
+    # score based on number of checkers in a row + empty spaces
     if player_count == 4:
         score += 1000
     elif player_count == 3 and empty_count == 1:
@@ -132,7 +145,11 @@ def score_window(window, player):
 
     return score
 
+# set depth to 7 by default so the game goes at a proper pace
 def minimax(board, depth=7):
+    """
+    Returns the optimal action for the current player.
+    """
     if terminal(board):
         return None
 
@@ -154,7 +171,7 @@ def minimax(board, depth=7):
 
     return optimal_move
 
-def max_value(board, depth):
+def max_value(board, depth, alpha=-math.inf, beta=math.inf):
     if terminal(board):
         return utility(board)
 
@@ -164,10 +181,13 @@ def max_value(board, depth):
     v = -math.inf
     for action in actions(board):
         v = max(v, min_value(result(board, action), depth - 1))
+        alpha = max(alpha, v)
+        if alpha >= beta:
+            break
 
     return v
 
-def min_value(board, depth):
+def min_value(board, depth, alpha=-math.inf, beta=math.inf):
     if terminal(board):
         return utility(board)
 
@@ -177,5 +197,8 @@ def min_value(board, depth):
     v = math.inf
     for action in actions(board):
         v = min(v, max_value(result(board, action), depth - 1))
+        beta = min(beta, v)
+        if alpha >= beta:
+            break
 
     return v
